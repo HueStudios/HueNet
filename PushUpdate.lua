335ed90c099e687d188db28bbfc102a60d666d33
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2019-07-15 21:05:21 -0500

    Add comments

--- PushUpdate.lua
+++ PushUpdate.lua
@@ -1,14 +1,16 @@
-
-
+--Library imports
 local computer = require "computer"
 local component = require "component"
 local modem = component.modem
 
+--Initialize modem
 modem.open(4200)
 modem.setStrength(400)
 
+--Obtain console arguments
 args = {...}
 
+--Load update information from disk and console
 local firmware_version = args[3]
 update_parts = {}
 update_file = io.open(args[2], "r")
@@ -22,22 +24,29 @@ while true do
 end
 update_file:close()
 
-
+--List of servers currently being updated
 updating = {}
 
 while true do
+  --Constantly broadcast firmware version
   modem.broadcast(4200, firmware_version)
   signal, _, from, port, _, remote_firmware_version, command = computer.pullSignal(1)
   if signal == "modem_message" and port == 4200 then
+
+    --Respond to messages asking for updates
     if command == "update_me" and remote_firmware_version < firmware_version then
       updating[from] = 1
       print(from, "asked for update", firmware_version)
     end
+
+    --Stop sending updates when completed
     if command == "update_received" then
       updating[from] = nil
       print(from, "finished update", firmware_version)
     end
   end
+
+  --Send update chunks to each client
   for k, v in pairs(updating) do
     if v > #update_parts then
       updating[k] = 1
